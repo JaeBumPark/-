866bd8d1d534026feedc3e3b012cbffa3d355026
JaeBumPark <kyontoki0209@gmail.com>
2023-03-15 19:53:50 +0900

    mountain

--- 섹션 3/Mountain.py
+++ 섹션 3/Mountain.py
@@ -1,13 +1,18 @@
 n=int(input())
-l= [list(map(int, input().split())) for _ in range (5)]
-l.insert(0, [0]*n) # 첫행에 0 박기
-l.append([0]*n)
-
-for x in l:
+l = [list(map(int, input().split())) for _ in range (n)]
+dx = [-1, 0, 1, 0] # 좌, 우 이동
+dy = [0,1, 0, -1] # 상, 하 이동
+cnt=0
+l.insert(0, [0]*n) #2차원 행렬 첫행으로 [0]*n 박음
+l.append([0]*n)    # 2차원 행렬 끝에 [0]
+for x in l: # x는 각 행
     x.insert(0,0)
     x.append(0)
-
-# for x in l:
-#     print(x, "봉우리 완성성")   
     
-for i in range( )     
+print(l)    
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if all(l[i][j]>l[i+dx[k]][j+dy[k]] for k in range(4)):
+           cnt += 1
+
+print(cnt)
