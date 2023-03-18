3bb92afb6526a1e7bbf32b7f62324c15632fb5ee
JaeBumPark <kyontoki0209@gmail.com>
2023-03-18 18:58:14 +0900

    section-5-단어찾기

--- 섹션 5/단어찾기 (hash)  (dictinary).py
+++ 섹션 5/단어찾기 (hash)  (dictinary).py
@@ -1,5 +1,15 @@
 #dictonary 단어 
 n=int(input())
+p= dict()
+for i in range(n):
+    a = input()
+    p[a]=1 # 키 값이 1이다!!
+for j in range(n-1):
+    b= input()
+    p[b]=0        
 
-        
-     
+print(p)
+for key,values in p.items() :
+    if values==1:
+       print(key)
+       break 
