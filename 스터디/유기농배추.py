39ca8a58f61a5837245d6f65b734196f466d32ce
JaeBumPark <kyontoki0209@gmail.com>
2024-08-25 17:23:47 +0900

    배추change

--- 스터디/유기농배추.py
+++ 스터디/유기농배추.py
@@ -1,23 +1,24 @@
+
+## 백준 채점 시스템에서 최대 재귀 깊이를 디폴트 값으로 1000으로 정해놓음
+##런타임 에러는 그 최대 깊이를 초과하여 재귀 호출을 하기 때문에 발생
+
 import sys
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+input = sys.stdin.readline  ##최대 깊이 limit을 위해
 
-# dfs 정의
-def dfs(x, y):
-    # 상하좌우
+def dfs(x, y): # dfs 정의
+
     dx = [0, 0, -1, 1] 
-    dy = [1, -1, 0, 0]
+    dy = [1, -1, 0, 0]      # 상하좌우
 
-    # 네 방향 탐색
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        # 범위 안에 있고 1이면(=배추이면) 지나간것을 -1로 표시하고 주변 탐색
         if (0 <= nx < m) and (0 <= ny < n) and graph[ny][nx] == 1:
-            graph[ny][nx] = -1
+            graph[ny][nx] = -1         # 범위 안에 있고 1이면(=배추이면) 지나간것을 -1로 표시하고 주변 탐색
             dfs(nx, ny)
 
-t = int(input()) # 테스트 케이스의 개수
+t = int(sys.stdin.readline()) # 테스트 케이스의 개수
 for _ in range(t):
     m, n, k = map(int, input().split()) # 가로, 세로, 배추 개수
     graph = [[0 for _ in range(m)] for _ in range(n)]
