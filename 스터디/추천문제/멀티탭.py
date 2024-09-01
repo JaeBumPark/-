05dc22a2a08871f4235b47298e73bb3612993e37
JaeBumPark <kyontoki0209@gmail.com>
2024-09-01 16:50:26 +0900

    multitap

--- 스터디/추천문제/멀티탭.py
+++ 스터디/추천문제/멀티탭.py
@@ -1,22 +1,36 @@
-
 import sys
 input = sys.stdin.readline
 
-N,K=map(int, sys.stdin.readline().split())   # 멀티탭 구멍개수, 사용횟수
-L=list(map(int, sys.stdin.readline().split()))
-code = []
-answer = 0
-priority = []
+# 멀티탭 구멍 개수 N, 전기 용품 사용 횟수 K
+N, K = map(int, input().split())
+L = list(map(int, input().split()))  # 전기 용품의 사용 순서
+code = []  # 현재 멀티탭에 꽂혀 있는 전기 용품
+answer = 0  # 플러그를 뽑는 횟수
 
 for i in range(K):
-    if L[i] in code :  # 코드에 이미 꽂혀져있음
+    if L[i] in code:     # 이미 멀티탭에 꽂혀 있는 기기라면 pass
+
         continue
 
-    if len(code)  !=  N :  # 코드 자리 남음
+  
+    if len(code) < N:   # 멀티탭에 자리가 남아있다면 새로운 기기를 꽂는다.
         code.append(L[i])
-        
-    for c in code:  # 꽂혀져 있는 코드들
-        if c in code[i:]: # 다음에 또 이용해야한다면
-            priority.append(code[i:].index(c))
-        else:
+        continue  # 자리가 남아 있을 때는 교체 로직으로 가지 않도록 한다.
+
+
+    # 멀티탭이 꽉 차있는 경우, 우선순위를 계산해서 가장 늦게 사용되거나 사용되지 않는 기기를 뽑는다.
+    priority = []  # 각 코드가 다음에 언제 사용되는지 기록
+    for c in code:
+        if c in L[i:]:  # 멀티탭에 꽂힌 기기가 앞으로도 사용될 경우
+            priority.append(L[i:].index(c))
+        else:  # 사용되지 않는다면 매우 큰 값을 넣어 우선적으로 뽑도록 설정
             priority.append(101)
+
+    # 가장 우선순위가 낮은 기기의 인덱스를 찾는다.
+    out_idx = priority.index(max(priority))
+    # 그 기기를 빼고 새로운 기기를 꽂는다.
+    code[out_idx] = L[i]
+    answer += 1
+
+# 최소 플러그를 뽑는 횟수 출력
+print(answer)
